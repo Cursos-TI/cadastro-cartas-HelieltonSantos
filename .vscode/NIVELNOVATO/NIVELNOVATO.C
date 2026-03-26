90d4d21fe18558dab3638904ed2425901bdce089
HelieltonSantos <helieltonn2018@Gmail.com>
2026-03-26 23:07:54 +0000

    Main Cartas super trunfo

--- .vscode/NIVELNOVATO/NIVELNOVATO.C
+++ .vscode/NIVELNOVATO/NIVELNOVATO.C
@@ -40,5 +40,39 @@ scanf("%f", &pib);
 printf("Digite Seus Pontos Turisticos:\n");
 scanf("%d", &pontosturisticos);
 
+#include 
+
+int carta2;                                             
+char estado2 [50] = "Céara";                             
+char Codigo2 [50] = "A02";                               
+char Cidade2 [50] = "Fortaleza";                               
+int população2 = 30000000;                                 
+float aréa2 = 16611.11;                                     
+float pib2 = 122333.22;                                    
+int pontosturisticos2 = 25;     
+
+printf(" Carta 2\n");
+
+printf("Digite o Estado:\n");
+scanf("%s" , &estado2);
+
+printf("Digite o Codigo:\n");
+scanf("%s" , &Codigo2);
+
+printf("Digite a Cidade:\n");
+scanf("%s" , &Cidade2);
+
+printf("Digite Sua População:\n");
+scanf("%d" , &população2);
+
+printf("Digite a aréa:\n");
+scanf("%f" , &aréa2);
+
+printf("Digite o pib2:\n");
+scanf("%f" , &pib2);
+
+printf("Pontos Turisticos:\n");
+scanf("%d" , &pontosturisticos2);
+
 
 }
